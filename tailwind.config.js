a08f5588c69df62e82c7e5c4abda7569c8adef07
Maksim Mikhailov <max@mer.pw>
2023-07-28 23:59:47 +0300

    Install daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
+
+import daisyUI from "daisyui";
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyUI],
 };
